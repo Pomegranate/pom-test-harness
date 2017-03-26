b4140ee8e291b852d584e57d21872750435715fe
Jim Bulkowski <jim.b@paperelectron.com>
2017-03-26 11:41:33 -0400

    initial

---
+++ index.js
@@ -0,0 +1,10 @@
+/**
+ * @file index
+ * @author Jim Bulkowski <jim.b@paperelectron.com>
+ * @project pom-test-stubs
+ * @license MIT {@link http://opensource.org/licenses/MIT}
+ */
+
+"use strict";
+
+exports.tapHarness = require('./lib/tapHarness')
